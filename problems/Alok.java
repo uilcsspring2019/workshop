3fda1d419b4b3db4ca5f4786e1a3655962256572
Chad Purdy <chpurdy@gmail.com>
2019-03-01 21:22:54 -0600

    done

---
+++ problems/Alok.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import static java.lang.System.*;
+
+public class Alok {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new File("alok.dat"));
+        int count = 0;
+        double total = 0;
+        while(in.hasNext()) {
+            total += in.nextDouble();
+            count++;
+        }
+        out.printf("$%.2f\n",total);
+        out.printf("$%.2f\n",total/count);
+    }
+}
