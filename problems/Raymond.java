ec597b6e98f54a2a6d5943eedfb5eec9e118bffc
Chad Purdy <chpurdy@gmail.com>
2019-03-03 16:32:29 -0600

    solution

---
+++ problems/Raymond.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import static java.lang.System.*;
+
+public class Raymond {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new File("raymond.dat"));
+        while(in.hasNext()) {
+            int n = in.nextInt();
+            out.printf("%d %d\n",n,~n);
+        }
+
+    }
+}
