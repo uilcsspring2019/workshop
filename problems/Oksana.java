710c163a32145d3df7e54ad3e6223a74272d746e
Chad Purdy <chpurdy@gmail.com>
2019-03-03 21:39:30 -0600

    solution

---
+++ problems/Oksana.java
@@ -0,0 +1,12 @@
+import static java.lang.System.*;
+
+public class Oksana {
+    public static void main(String[] args) {
+        for(int i = 0;i<8;i++) {
+            for(int j = 0; j < 8; j++) {
+                out.print("* ");
+            }
+            out.println();
+        }
+    }
+}
